9b6d4a629850cb1a2669ed99240491c64f5f1299
Krishna Mishra <er.krishna.mishra@gmail.com>
2024-03-05 19:56:11 +0530

    changes

--- app/Models/InformationPageManagement.php
+++ app/Models/InformationPageManagement.php
@@ -23,4 +23,35 @@ class InformationPageManagement extends Model
         $this->attributes['slug'] = Str::slug($value, '-');
     }
 
+
+     protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($page) {
+
+            if ($page->name) {
+                $g_slug = Str::slug($page->name);
+                $check_slug = InformationPageManagement::where('name', $page->name)->count();
+                if ($check_slug > 0) {
+                    $page->slug = Str::slug($page->name.'-'. ($check_slug));
+                } else {
+                    $page->slug = $g_slug;
+                };
+            }
+        });
+
+        static::updating(function ($page) 
+        {
+            $g_slug = Str::slug($page->name);
+            $check_slug = InformationPageManagement::where('name', $page->name)->where('id', '!=', $page->id)->count();
+            if ($check_slug > 0) {
+                $page->slug = Str::slug($page->name.'-'. ($check_slug));
+            } else {
+                $page->slug = $g_slug;
+            };
+        });
+
+
+    }
+
 }
