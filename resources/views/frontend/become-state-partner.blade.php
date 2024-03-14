c0699b98a3673e90acad060ff317f9c187418f10
Krishna Mishra <er.krishna.mishra@gmail.com>
2024-03-14 23:46:47 +0530

    changes

--- resources/views/frontend/become-state-partner.blade.php
+++ resources/views/frontend/become-state-partner.blade.php
@@ -38,7 +38,7 @@ $setting =  \App\Models\WebsiteContactSetting::first();
                             </div>
                             @endif
 
-                            <form class="row col-md-10" method="POST" action="{{ route('save-become-state-partner') }}" id="contactForm">
+                            <form class="row col-md-8" method="POST" action="{{ route('save-become-state-partner') }}" id="contactForm">
                                 @csrf
                                 @method('POST')
                                 <input type="hidden" value="become_state_partner" name="form_type">
@@ -64,8 +64,8 @@ $setting =  \App\Models\WebsiteContactSetting::first();
                                 <div style="display: none;" class="form-group {{ $errors->has('brandagreement') ? 'has-error' : '' }} mb-2">
                                     <label for="basicText" class="form-label">Have standard Brand Agreement</label>
                                     <div class="col-sm-12">
-                                        <input id="brandagreement" type="radio" value="Yes"   name="brandagreement" style=" margin:10px 0 0;"  > &nbsp;&nbsp;Yes &nbsp;&nbsp;
-                                        <input id="brandagreement" type="radio" value="No"  name="brandagreement"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;No
+                                        <input id="brandagreement" type="radio" value="Yes"   name="brandagreement" style=" margin:10px 0 0;"  > &nbsp;Yes &nbsp;&nbsp;
+                                        <input id="brandagreement" type="radio" value="No"  name="brandagreement"  style=" margin:10px 0 0;"  checked > &nbsp;No
                                     </div>
                                 </div>
 
@@ -73,8 +73,8 @@ $setting =  \App\Models\WebsiteContactSetting::first();
                                 <div class="form-group {{ $errors->has('business_experience') ? 'has-error' : '' }} mb-2">
                                     <label for="basicText" class="form-label">Do you have any experience in operating similar business</label>
                                     <div class="col-sm-12">
-                                        <input id="business_experience" type="radio" value="Yes"   name="business_experience" style=" margin:10px 0 0;"  > &nbsp;&nbsp;Yes &nbsp;&nbsp;
-                                        <input id="business_experience" type="radio" value="No"  name="business_experience"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;No
+                                        <input id="business_experience" type="radio" value="Yes"   name="business_experience" style=" margin:10px 0 0;"  > &nbsp;Yes &nbsp;&nbsp;
+                                        <input id="business_experience" type="radio" value="No"  name="business_experience"  style=" margin:10px 0 0;"  checked > &nbsp;No
                                     </div>
                                 </div>
 
@@ -82,9 +82,9 @@ $setting =  \App\Models\WebsiteContactSetting::first();
                                 <div class="form-group {{ $errors->has('investment_range') ? 'has-error' : '' }} mb-2">
                                     <label for="basicText" class="form-label">Investment Range</label>
                                     <div class="col-sm-12">
-                                        <input id="investment_range" type="radio" value="Below 3 Lacs "   name="investment_range" style=" margin:10px 0 0;"  > &nbsp;&nbsp;Below 3 Lacs  &nbsp;&nbsp;
-                                        <input id="investment_range" type="radio" value="3 to 5 Lacs"  name="investment_range"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;3 to 5 Lacs &nbsp;&nbsp;
-                                        <input id="investment_range" type="radio" value="5 Lacs+"  name="investment_range"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;5 Lacs+
+                                        <input id="investment_range" type="radio" value="Below 3 Lacs "   name="investment_range" style=" margin:10px 0 0;"  > &nbsp;Below 3 Lacs  &nbsp;&nbsp;
+                                        <input id="investment_range" type="radio" value="3 to 5 Lacs"  name="investment_range"  style=" margin:10px 0 0;"  checked > &nbsp;3 to 5 Lacs &nbsp;&nbsp;
+                                        <input id="investment_range" type="radio" value="5 Lacs+"  name="investment_range"  style=" margin:10px 0 0;"  checked > &nbsp;5 Lacs+
                                     </div>
                                 </div>
 
@@ -92,22 +92,22 @@ $setting =  \App\Models\WebsiteContactSetting::first();
                                 <div class="form-group {{ $errors->has('associate_time') ? 'has-error' : '' }} mb-2">
                                     <label for="basicText" class="form-label">How soon do you want to associate:</label>
                                     <div class="col-sm-12">
-                                        <input id="associate_time" type="radio" value="Immediately"   name="associate_time" style=" margin:10px 0 0;"  > &nbsp;&nbsp;Immediately &nbsp;&nbsp;
-                                        <input id="associate_time" type="radio" value="Within 3 Months"  name="associate_time"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;Within 3 Months &nbsp;&nbsp;
-                                        <input id="associate_time" type="radio" value="After 3 Months"  name="associate_time"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;After 3 Months
+                                        <input id="associate_time" type="radio" value="Immediately"   name="associate_time" style=" margin:10px 0 0;"  > &nbsp;Immediately &nbsp;&nbsp;
+                                        <input id="associate_time" type="radio" value="Within 3 Months"  name="associate_time"  style=" margin:10px 0 0;"  checked > &nbsp;Within 3 Months &nbsp;&nbsp;
+                                        <input id="associate_time" type="radio" value="After 3 Months"  name="associate_time"  style=" margin:10px 0 0;"  checked > &nbsp;After 3 Months
                                     </div>
                                 </div>
 
                                 <div class="form-group {{ $errors->has('how_to_know_me') ? 'has-error' : '' }} mb-2">
                                     <label for="basicText" class="form-label">How did you get to know about FranchiseBazaar partnership program:</label>
                                     <div class="col-sm-12">
-                                        <input id="how_to_know_me" type="radio" value="Social Media"   name="how_to_know_me" style=" margin:10px 0 0;"  > &nbsp;&nbsp;Social Media &nbsp;&nbsp;
-                                        <input id="how_to_know_me" type="radio" value="Referral"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;Referral &nbsp;&nbsp;
-                                        <input id="how_to_know_me" type="radio" value="Webinars"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;Webinars &nbsp;&nbsp;
-                                        <input id="how_to_know_me" type="radio" value="Partner Companies"   name="how_to_know_me" style=" margin:10px 0 0;"  > &nbsp;&nbsp;Partner Companies &nbsp;&nbsp;
-                                        <input id="how_to_know_me" type="radio" value="Newspaper"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;Newspaper &nbsp;&nbsp;
-                                        <input id="how_to_know_me" type="radio" value="TV Advertisement"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;TV Advertisement  &nbsp;&nbsp;                                      
-                                        <input id="how_to_know_me" type="radio" value="Others"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;&nbsp;Others
+                                        <input id="how_to_know_me" type="radio" value="Social Media"   name="how_to_know_me" style=" margin:10px 0 0;"  > &nbsp;Social Media &nbsp;&nbsp;
+                                        <input id="how_to_know_me" type="radio" value="Referral"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;Referral &nbsp;&nbsp;
+                                        <input id="how_to_know_me" type="radio" value="Webinars"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;Webinars &nbsp;&nbsp;
+                                        <input id="how_to_know_me" type="radio" value="Partner Companies"   name="how_to_know_me" style=" margin:10px 0 0;"  > &nbsp;Partner Companies &nbsp;&nbsp;
+                                        <input id="how_to_know_me" type="radio" value="Newspaper"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;Newspaper &nbsp;&nbsp;
+                                        <input id="how_to_know_me" type="radio" value="TV Advertisement"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;TV Advertisement  &nbsp;&nbsp;                                      
+                                        <input id="how_to_know_me" type="radio" value="Others"  name="how_to_know_me"  style=" margin:10px 0 0;"  checked > &nbsp;Others
                                     </div>
                                 </div>
 
